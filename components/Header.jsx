8a4175540ad492ac6d12c28bd08e0e8b869de32d
MrFranksJr <frankyjr@me.com>
2023-07-24 18:38:17 +0200

    testing

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import logo from "../images/react-logo.png"
+import { Navigation } from "./NavBar.jsx"
 
 export default Header
 
@@ -12,15 +13,5 @@ function Header() {
             </div>
             <h1>Fun facts about React</h1>
         </header>
-    )
-}
-
-function Navigation() {
-    return (
-        <ul className="nav-items">
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
     )
 }
---
+++ components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export { Navigation }
+
+function Navigation() {
+    return (
+        <ul className="nav-items">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    )
+}
