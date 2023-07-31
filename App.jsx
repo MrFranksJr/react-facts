bb792c6d817295b28535c345b1c0b07f8df28669
MrFranksJr <frankyjr@me.com>
2023-07-31 16:49:25 +0200

    updates

--- App.jsx
+++ App.jsx
@@ -1,23 +1,17 @@
 import React from "react"
-import ReactDOM from "react-dom/client"
-import "./index.css"
-
 import Header from "./components/Header.jsx"
 import Footer from "./components/Footer.jsx"
 import AppContent from "./components/AppContent.jsx"
 
+export { App }
+
 
 function App() {
     return (
-        <div id="core">
+        <div className="container">
             <Header />
             <AppContent />
             <Footer />
         </div>
     )
 }
-
-
-const rootLevel = ReactDOM.createRoot(document.getElementById('root'))
-
-rootLevel.render(<App />)
